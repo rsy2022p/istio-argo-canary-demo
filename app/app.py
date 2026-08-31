23f104b6fd9d02bc5b3350e2456c129b44c6f3cd
Swathi <132664537+rsy2022p@users.noreply.github.com>
2026-08-31 16:47:42 +0530

    modified app.py

--- app/app.py
+++ app/app.py
@@ -25,7 +25,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header("Content-Type", "text/plain")
         self.end_headers()
 
-        response = f"PRODUCT VERSION {VERSION}\n"
+        response = f"PRODUCT VERSION V6-CANARY {VERSION}\n"
         self.wfile.write(response.encode())
 
 
