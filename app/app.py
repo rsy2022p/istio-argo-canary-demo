3ad8702223357bef2024753af885c144b6b0c37b
Swathi <132664537+rsy2022p@users.noreply.github.com>
2026-08-31 17:20:30 +0530

    updated app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 
-VERSION = os.getenv("VERSION", "v7")
+VERSION = os.getenv("VERSION", "v8")
 
 
 class Handler(BaseHTTPRequestHandler):
